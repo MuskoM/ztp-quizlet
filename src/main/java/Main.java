d74d635ce0e8b8945aab448c990e1a6e31e61c96
nvi0 <muskomatt@outlook.com>
2020-12-25 23:02:49 +0100

    resources folder added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ import org.bson.Document;
 
 import javax.xml.crypto.Data;
 import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Database database = Database.getInstance();
